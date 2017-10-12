ec2f0ad87ffc076c06782a24792587870e99c3e2
italoricci <italoricci@gmail.com>
2017-10-11 23:11:40 -0300

    [Node]Inclusão do Express, EJS e habilitado o uso do Nodemon.

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/produtos', function (resquest, response) {
+    console.log('Chegou o request');
+    response.render('produtos/lista');
+})
+
+app.listen(3000, function () {
+    console.log('Servidor Up!');
+});
